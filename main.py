bc697c9d90501eaf02be8a554cfb959b5749d23a
David Olowomeye <olowomeyedavid242@gmail.com>
2023-11-06 12:50:19 +0000

    Refactor the code with OOP

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+from snake import  Snake
 from turtle import Screen, Turtle
 
 screen = Screen()
@@ -7,17 +8,8 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-# To set the snake starting position
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-segments = []
-
-for position in starting_positions:
-    new_segment = Turtle(shape="square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+snake = Snake()
 
 screen.update()
 
@@ -26,12 +18,9 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    # This wil make the snake move forward automatically
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
+    snake.move_snake()
+
+
 
 
 
