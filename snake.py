f832fd45dabe6e15c014e906fd159707088a56d9
David Olowomeye <olowomeyedavid242@gmail.com>
2023-11-06 13:29:58 +0000

    Added the key controlling features to the game

--- snake.py
+++ snake.py
@@ -4,10 +4,19 @@ from turtle import Turtle
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 
+# To maintain the movement direction
+RIGHT = 0
+UP = 90
+LEFT = 180
+DOWN = 270
+
+
+
 class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.snake_head = self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -22,4 +31,21 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-            self.segments[0].forward(MOVE_DISTANCE)
+
+        self.snake_head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.snake_head.heading() != DOWN:
+            self.snake_head.setheading(UP)
+
+    def down(self):
+        if self.snake_head.heading() != UP:
+            self.snake_head.setheading(DOWN)
+
+    def left(self):
+        if self.snake_head.heading() != RIGHT:
+            self.snake_head.setheading(LEFT)
+
+    def right(self):
+        if self.snake_head.heading() != LEFT:
+            self.snake_head.setheading(RIGHT)
